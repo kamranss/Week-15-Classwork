8d795ab941d7d30e26fe60d5fb82c6af4836ab87
kamranss <kamranss@code.edu.az>
2022-12-25 19:45:36 +0400

    Feature: Console App started

---
+++ Domain/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    internal class Student : IEntity
+    {
+        public int Id { get ; set ; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+    }
+}

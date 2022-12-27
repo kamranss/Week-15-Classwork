43a2ff5c8cfe08067042745b8ec12833083cbc85
kamranss <kamranss@code.edu.az>
2022-12-27 20:59:15 +0400

    Refactor: Project updated added service

---
+++ Service/Interfaces/IGroupService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Service.Interfaces
+{
+    internal interface IGroupService
+    {
+        Group Create(Group group);
+        Group Update(int id, Group group);
+        Group Delete(int id);
+        Group Get(int id);
+        Group Get(string Name);
+        List<Group> GetAll(string name);
+    }
+}
---
+++ Service/Service/GroupService.cs
@@ -0,0 +1,78 @@
+﻿using DataAccess.Repositories;
+using Service.Interfaces;
+using System;
+using Domain.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Service
+{
+    internal class GroupService : IGroupService
+    {
+        private readonly GroupRepository groupRepository;
+        public static int Id { get; set; }
+        public GroupService()
+        {
+            groupRepository = new GroupRepository();
+        }
+        public Group Create(Group group)
+        {
+            try
+            {
+                Group existgroup = groupRepository.Get(g => g.GroupName == group.GroupName);
+                if (existgroup==null)
+                {
+                    group.Id = Id;
+                    if (groupRepository.Create(group))
+                    {
+                        Id++;
+                        return group;
+                    }
+                    return null;
+                }
+                return null;
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+
+        public Group Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Group Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Group Get(string Name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Group> GetAll(string name)
+        {
+            try
+            {
+                List<Group> groups = groupRepository.GetAll(g => g.GroupName == name);
+                return groups;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Group Update(int id, Group group)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

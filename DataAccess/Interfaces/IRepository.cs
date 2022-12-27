253215396c2f87b38cfe4acf1f020ae3b84c1b68
kamranss <kamranss@code.edu.az>
2022-12-27 14:54:04 +0400

    Refactor: Project Updated addaed fake DataAccess

--- DataAccess/Interfaces/IRepository.cs
+++ DataAccess/Interfaces/IRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Interfaces
 {
-    internal class IRepository
+    internal interface IRepository<T> where T : IEntity
     {
+        bool Create(T entity);
+        bool Update(T entity);
+        bool Delete(T entity);
+        T Get(Predicate<T> filter = null);
+        List<T> GetAll(Predicate<T> filter = null);
     }
 }
---
+++ DataAccess/Repositories/GroupRepository.cs
@@ -0,0 +1,38 @@
+﻿using DataAccess.Interfaces;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class GroupRepository : IRepository<Group>
+    {
+        public bool Create(Group entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Group entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Group Get(Predicate<Group> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Group> GetAll(Predicate<Group> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Group entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

96bfe9cc4198cbfdf2c62ed05ced9df2588a0ccc
kamranss <kamranss@code.edu.az>
2023-01-01 22:56:38 +0400

    Refactor: added Student Service

---
+++ Service/Service/StudentService.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Models;
+using Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Service
+{
+    public class StudentService : IStudentService
+    {
+        public Student Create(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student Delete(int id, Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student Get(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Student> GetAll(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student Update(Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
